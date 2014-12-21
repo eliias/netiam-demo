6495572d256a7a1a360ca9ef9544038f28f0199a
Hannes Moser <hannes@impossiblearts.com>
2014-12-21 23:14:11 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var express = require( 'express' ),
+    netiam  = require( 'netiam' ),
+    app     = express(),
+    server  = require( 'http' ).createServer( app ),
+    netiam  = netiam( app );
+
+netiam
+    .get( '/' )
+    .custom( function( resource, req, res ) {
+        resource.data = {
+            'Hello': 'World!'
+        };
+    } )
+    .json();
+
+server.listen( 3000 );
