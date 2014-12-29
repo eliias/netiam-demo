556fdfd7f421ac830b9d5991bff65a30b1c2761e
Hannes Moser <hannes@impossiblearts.com>
2014-12-29 16:47:31 +0100

    demo: all REST commands are working

--- app.js
+++ app.js
@@ -1,28 +1,40 @@
 'use strict';
 
-var express = require( 'express' ),
-    app     = express(),
-    server  = require( 'http' ).createServer( app ),
-    netiam  = require( 'netiam' )( app ),
-    User    = require( './models/user' );
+var express    = require( 'express' ),
+    bodyParser = require( 'body-parser' ),
+    app        = express(),
+    server     = require( 'http' ).createServer( app ),
+    netiam     = require( 'netiam' )( app ),
+    User       = require( './models/user' );
 
 // Database
 require( './modules/db' );
 
+app.use( bodyParser.json() );
+
 netiam
-    .get( '/users' )
-    .rest( {
-        model:       User,
-        idAttribute: '_id'
-    } )
+    .get( '/users', {model: User} )
+    .rest()
     .json();
 
 netiam
-    .get( '/users/:id' )
-    .rest( {
-        model:       User,
-        idAttribute: '_id'
-    } )
+    .post( '/users', {model: User} )
+    .rest()
+    .json()
+
+netiam
+    .get( '/users/:id', {model: User} )
+    .rest()
+    .json();
+
+netiam
+    .put( '/users/:id', {model: User} )
+    .rest()
+    .json();
+
+netiam
+    .delete( '/users/:id', {model: User} )
+    .rest()
     .json();
 
 server.listen( 3000 );
