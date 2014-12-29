0e63966fe371b337ee1e0aaf896fd8f53396789f
Hannes Moser <hannes@impossiblearts.com>
2014-12-30 00:57:57 +0100

    chore: adapt app to lib changes

--- app.js
+++ app.js
@@ -10,24 +10,42 @@ var express    = require( 'express' ),
 // Database
 require( './modules/db' );
 
+app.enable( 'trust proxy' );
 app.use( bodyParser.json() );
+
+// Error handling
+app.use( function( err, req, res, next ) {
+    res
+        .status( 500 )
+        .json( {
+            code:    500,
+            status:  'INTERNAL SERVER ERROR',
+            message: err.message,
+            data:    err.stack
+        } );
+} );
 
 netiam
     .get( '/users' )
     .rest( {model: User} )
     .profile( {query: 'profile'} )
-    .json();
+    //.json();
 
 netiam
     .post( '/users' )
     .rest( {model: User} )
     .profile( {query: 'profile'} )
-    .json()
+    .json();
 
 netiam
     .get( '/users/:id' )
     .rest( {model: User} )
     .profile( {query: 'profile'} )
+    .transform( function() {
+        var body = this.body();
+        body.password = undefined;
+        this.body( body );
+    } )
     .json();
 
 netiam
@@ -40,5 +58,14 @@ netiam
     .delete( '/users/:id' )
     .rest( {model: User} )
     .json();
+
+// Catch all
+app.use( function( req, res ) {
+    res
+        .json( {
+            code:    404,
+            status:  'DOCUMENT NOT FOUND'
+        } );
+} );
 
 server.listen( 3000 );
