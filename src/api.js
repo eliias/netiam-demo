03c3fc3587f193e6cd9137a2e87b6a2c084184fb
Hannes Moser <hannes@impossiblearts.com>
2016-03-14 10:26:12 +0100

    feat: add application

---
+++ src/api.js
@@ -0,0 +1,11 @@
+import auth from 'netiam-contrib-auth'
+import json from 'netiam-contrib-json'
+import oauth from 'netiam-contrib-oauth'
+import rest from 'netiam-contrib-rest'
+
+export const plugins = {
+  auth,
+  json,
+  oauth,
+  rest
+}
